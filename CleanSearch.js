fe56ff3c5af699aa97e372b6c9980b12cc549798
Mando <30785368+MandoCSGO@users.noreply.github.com>
2024-08-26 21:28:04 +0300

    Update and rename BeautySearch.js to CleanSearch.js

---
+++ CleanSearch.js
@@ -0,0 +1,2 @@
+sa_config.enableTwoPanesZI = false;
+sa_config.enabledDataSources = ["DFLS","CUSE","QSCH","WWE","PP","IBA","ST","FEH","Web","OSTMA","QS","IFF","MPVD","MDOC","MFOL","MPHO","MVID","MMUS","ODC","MRU","MPP","MST","MFF","MRS","CG","PT","SSUE","SSUC","SSEE","SSEC","TS","ANA"];
